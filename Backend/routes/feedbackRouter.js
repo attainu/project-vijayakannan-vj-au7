9252b537cdf7547a44b59350e4c22383e02acd98
salim mahat <salimmahat22@gmail.com>
2020-10-05 10:11:09 +0530

    update code in backend

--- Backend/routes/feedbackRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-
-const router = Router();
-
-const { feedback } = require("../controllers/feedbackController");
-
-router.post("/feedback", feedback);
-
-module.exports = router;
